2e75c6d58868467108109555c633ef887badf7a5
Ronald Marques <ronaldpmarques01@gmail.com>
2022-03-09 19:44:20 -0300

    Usando mirage js

--- dtmoney/src/components/Header/index.tsx
+++ dtmoney/src/components/Header/index.tsx
@@ -1,14 +1,23 @@
 import logoImg from "../../assets/logo.svg"
+
+
 import { Container, Content } from "./styles"
 
-export function Header(){
+interface Headerprops {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: Headerprops){
+
     return(
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
+
+               
             </Content>
         </Container>
     )
